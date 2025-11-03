a2a765b54e7dc80734ed9e977575ab0fcf0063eb
Sabrina Bessa <saabimm@gmail.com>
2025-11-03 16:05:50 +0100

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import FormulaireCNOA from './Pages/Sign-up'
-import UserProvider from "./context/UserContext.jsx";
 import ProfilePage from './Pages/ProfilePage'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default function App() {
