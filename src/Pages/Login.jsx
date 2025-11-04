39c313662f1588e93f55847e0d2b4ec7dc605c50
SaaImm <saabimm@gmail.com>
2025-11-04 13:18:20 +0100

    url change

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -22,7 +22,7 @@ const { setUser } = useContext(UserContext);
   const handleSubmit =async (e)=> {
     e.preventDefault();
     try{
-      const response = await fetch("http://localhost:3000/auth/login" ,{
+      const response = await fetch("https://back-end-signup-and-login.onrender.com/auth/login" ,{
       method: "POST",
       headers: {
           "Content-Type": "application/json",
--- src/Pages/Sign-up.jsx
+++ src/Pages/Sign-up.jsx
@@ -28,7 +28,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:3000/auth/signup", {
+    const response = await fetch("https://back-end-signup-and-login.onrender.com/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
